8aab321a219a43851f7f54a4eefb0daaa61813a4
Agnieszka <aagnieszka.zurek@gmail.com>
2023-01-02 14:05:13 +0100

    downloading date and month from the calendar

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const calendar = document.getElementById("birth-date-widget");
 const nameField = document.getElementById("name-field");
 const showButton = document.getElementById("show-button");
+
+async function getBirthDayAndMonth() {
+  const calendar = document.getElementById("birth-date-widget");
+  const chosenDate = calendar.value;
+  const dateFormat = chosenDate.slice(5).replace("-", ".");
+  console.log("1");
+  // const zodiacSigns = await assignZodiacSignToDate(dateFormat);
+  // console.log(zodiacSigns);
+
+  console.log(dateFormat);
+  calendar.addEventListener("click", getBirthDayAndMonth);
+}
+getBirthDayAndMonth();
