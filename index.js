c61c6021bf048b804274e635a6f8da777f9b3b19
Agnieszka <aagnieszka.zurek@gmail.com>
2023-01-09 15:46:51 +0100

    onUserInput function added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const nameField = document.getElementById("name-field");
 const showButton = document.getElementById("show-button");
 const calendar = document.getElementById("birth-date-widget");
+console.log(calendar.value);
 
 function getBirthDayAndMonth() {
   const chosenDate = new Date(calendar.value);
@@ -21,3 +22,25 @@ async function showHoroscope() {
   console.log(horoscopeResult);
 }
 calendar.addEventListener("change", showHoroscope);
+
+function onUserInput() {
+  if (
+    nameField.value !== null &&
+    nameField.value !== undefined &&
+    nameField.value !== "" &&
+    calendar.value !== ""
+  ) {
+    showButton.disabled = false;
+  } else {
+    showButton.disabled = true;
+  }
+}
+nameField.addEventListener("input", onUserInput);
+
+// function showZodiacSign() {
+// const signHolder = document.getElementsByClassName("sign-holder")[0];
+// const horoscopeSign = getBirthDayAndMonth();
+//   const nameHolder = document.getElementsByClassName("name-holder")[0];
+//   nameHolder.innerHTML = nameField.value;
+// }
+// showZodiacSign();
